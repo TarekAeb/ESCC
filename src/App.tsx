66fcbb97831c56b0972e7a67523c18409d49faa1
TarekAeb <t6tarek@gmail.com>
2024-10-08 19:46:14 +0100

    fixing the register route

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import Lenis from '@studio-freight/lenis';
 import { useGSAP } from '@gsap/react';
 // import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './views/LandingPage';
+// import LandingPage from './views/LandingPage';
 import Register from './views/Register';
-import { useState } from 'react';
-import Loader from './components/Loader';
+// import { useState } from 'react';
+// import Loader from './components/Loader';
 const App = () => {
-  const [showLoader, setShowLoader] = useState(true);
-  setTimeout(() => {
-    setShowLoader(false);
-  }
-    , 17000);
+  // const [showLoader, setShowLoader] = useState(true);
+  // setTimeout(() => {
+  //   setShowLoader(false);
+  // }
+  //   , 17000);
   useGSAP(() => {
     // Create a new Lenis instance with the desired configuration
     const lenis = new Lenis({
@@ -35,8 +35,8 @@ const App = () => {
     <Router>
       {/* {showLoader ? <Loader /> : ( */}
         <Routes>
-          <Route path='/register' element={<LandingPage />} />
-          {/* <Route path='/register' element={<Register />} /> */}
+          {/* <Route path='/' element={<LandingPage />} /> */}
+          <Route path='/register' element={<Register />} />
         </Routes>
       {/* )} */}
     </Router>
